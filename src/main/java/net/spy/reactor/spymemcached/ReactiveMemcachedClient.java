a2150bd36da0b49e9cc336e1d4b81eed91fec0ae
linux_china <libing.chen@gmail.com>
2019-10-03 12:14:40 -0700

    Reactive Memcached Client

---
+++ src/main/java/net/spy/reactor/spymemcached/ReactiveMemcachedClient.java
@@ -0,0 +1,45 @@
+package net.spy.reactor.spymemcached;
+
+import net.spy.memcached.MemcachedClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Reactive Memcached Client
+ *
+ * @author linux_china
+ */
+public class ReactiveMemcachedClient implements SpymemcachedAdapter {
+    private MemcachedClient memcachedClient;
+
+    public ReactiveMemcachedClient(MemcachedClient memcachedClient) {
+        this.memcachedClient = memcachedClient;
+    }
+
+    Mono<Boolean> set(final String key, final int exp, final Object o) {
+        return operationFutureToMono(memcachedClient.set(key, exp, o));
+    }
+
+    Mono<Object> get(final String key) {
+        return getFutureToMono(memcachedClient.asyncGet(key));
+    }
+
+    Flux<Map.Entry<String, Object>> getBulk(Collection<String> keys) {
+        return bulkFutureToFlux(memcachedClient.asyncGetBulk(keys));
+    }
+
+    Flux<Map.Entry<String, Object>> getBulk(String... keys) {
+        return bulkFutureToFlux(memcachedClient.asyncGetBulk(keys));
+    }
+
+    Mono<Boolean> delete(String key) {
+        return operationFutureToMono(memcachedClient.delete(key));
+    }
+
+    Mono<Boolean> touch(final String key, final int exp) {
+        return operationFutureToMono(memcachedClient.touch(key, exp));
+    }
+}
