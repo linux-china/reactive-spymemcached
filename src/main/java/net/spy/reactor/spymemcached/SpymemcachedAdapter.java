2fbfce9f678291cde8c1082f52c4969ac4e5794a
linux_china <libing.chen@gmail.com>
2019-10-03 12:16:45 -0700

    Spymemcached Reactor Adapter

---
+++ src/main/java/net/spy/reactor/spymemcached/SpymemcachedAdapter.java
@@ -0,0 +1,59 @@
+package net.spy.reactor.spymemcached;
+
+import net.spy.memcached.internal.BulkFuture;
+import net.spy.memcached.internal.GetFuture;
+import net.spy.memcached.internal.OperationFuture;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+/**
+ * Spymemcached Reactor Adapter
+ *
+ * @author linux_china
+ */
+public interface SpymemcachedAdapter {
+
+    default Mono<Object> getFutureToMono(final GetFuture<Object> getFuture) {
+        return Mono.create(monoSink -> {
+            getFuture.addListener(future -> {
+                if (future.isCancelled()) {
+                    monoSink.error(new Exception("Cancelled"));
+                } else {
+                    if (future.getStatus().isSuccess()) {
+                        monoSink.success(future.get());
+                    } else {
+                        monoSink.error(new Exception("Failed"));
+                    }
+                }
+            });
+        });
+    }
+
+    default <T> Mono<T> operationFutureToMono(OperationFuture<T> operationFuture) {
+        return Mono.create(monoSink -> {
+            operationFuture.addListener(future -> {
+                if (future.isCancelled()) {
+                    monoSink.error(new Exception("Cancelled"));
+                } else {
+                    monoSink.success((T) future.get());
+                }
+            });
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    default Flux<Map.Entry<String, Object>> bulkFutureToFlux(final BulkFuture<Map<String, Object>> bulkFuture) {
+        return Flux.create(entryFluxSink -> {
+            bulkFuture.addListener(future -> {
+                if (future.getStatus().isSuccess()) {
+                    ((Map<String, Object>) future.get()).entrySet().forEach(entryFluxSink::next);
+                    entryFluxSink.complete();
+                } else {
+                    entryFluxSink.error(new Exception("Failed"));
+                }
+            });
+        });
+    }
+}
