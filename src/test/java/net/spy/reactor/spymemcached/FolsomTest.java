ada5946739dc925ee6a44fb0507b5888251f4059
linux_china <libing.chen@gmail.com>
2020-05-02 11:43:44 -0700

    Folsom test

---
+++ src/test/java/net/spy/reactor/spymemcached/FolsomTest.java
@@ -0,0 +1,40 @@
+package net.spy.reactor.spymemcached;
+
+import com.spotify.folsom.ConnectFuture;
+import com.spotify.folsom.MemcacheClient;
+import com.spotify.folsom.MemcacheClientBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ * Folsom test
+ *
+ * @author linux_china
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class FolsomTest {
+    private MemcacheClient<String> client;
+
+    @BeforeAll
+    public void setUp() throws Exception {
+        client = MemcacheClientBuilder.newStringClient()
+                .withAddress("127.0.0.1")
+                .connectAscii();
+        // make we wait until the client has connected to the server
+        ConnectFuture.connectFuture(client).toCompletableFuture().get();
+    }
+
+    @AfterAll
+    public void tearDown() {
+        client.shutdown();
+    }
+
+    @Test
+    public void testOperation() throws Exception {
+        client.set("key", "value", 10000).toCompletableFuture().get();
+        String value = client.get("key").toCompletableFuture().get();
+        System.out.println(value);
+    }
+}
